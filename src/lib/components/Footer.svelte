32a6a992b27b4bed277a08ea8b4c88b145597fa0
Toon-van-Berkel <toonvanberkel203@gmail.com>
2025-09-29 13:30:21 +0200

    footer component

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,3 +1,30 @@
-<footer>
-    <span>Toonvb - Toon van Berkel</span>
+<footer class="footer">
+    <div class="footer__container">
+        <div>
+            <span aria-label="Copyright">© 2025-2026 - Toonvb (Toon van Berkel) - All rights reserved.</span>
+            <div>
+                <span>All socials:</span>
+                <ul>
+                    <li>LinkedIn - <a href="https://www.linkedin.com/in/toon-van-berkel-a9112628b/" target="_blank">Toon van Berkel on LinkedIn</a></li>
+                    <li>Github - <a href="https://github.com/toon-van-berkel" target="_blank">Toon van Berkel on Github</a></li>
+                </ul>
+            </div>
+            <p>
+                To get in touch with Toon van Berkel, please email
+                <a href="mailto:toonvanberkel203@gmail.com">toonvanberkel203@gmail.com</a>.
+                Email is the preferred method of contact, as phone calls are often missed.
+                If you wish to call, please email your phone number first so it can be recognized.
+            </p>
+        </div>
+        <div>
+            <div>
+                <span>Informational pages:</span>
+                <span>Content soon to come...</span>
+            </div>
+            <div>
+                <span>DIY Projects</span>
+                <span>Content soon to come...</span>
+            </div>
+        </div>
+    </div>
 </footer>
