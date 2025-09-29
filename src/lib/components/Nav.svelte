4856f7b05b2a501528f3ad30148db3afa6157622
Toon-van-Berkel <toonvanberkel203@gmail.com>
2025-09-29 13:30:42 +0200

    navbar components

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,10 +1,17 @@
+<script>
+    const logo = '/images/toonvb-logo.svg';
+</script>
+
 <nav class="nav">
     <div class="nav__container">
         <div>
-
+            <img src="{logo}" alt="a circular logo of the letters t, v and a b symbolizing toonvb">
+            <span>Toonvb</span>
         </div>
         <ul>
-            <li></li>
+            <li>Projects</li>
+            <li>About</li>
+            <li>Contact</li>
         </ul>
     </div>
 </nav>
